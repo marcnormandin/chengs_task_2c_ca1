9890ace22938ed2967e96050f3e33f32c0914fab
Marc Normandin <marcnormandin@users.noreply.github.com>
2024-03-21 15:16:58 -0500

    Update load_figure_config.m

--- figure_scripts/load_figure_config.m
+++ figure_scripts/load_figure_config.m
@@ -14,10 +14,16 @@ CHENGS_TASK_2C_FIGURES_CONFIG.FIGURE_OUTPUT_FOLDER = 'D:\chengs_task_2c_ca1_proj
 % Set these to where the github repos are saved
 CHENGS_TASK_2C_FIGURES_CONFIG.MULANA_FOLDER = 'D:\chengs_task_2c_ca1_project\codes\MuLaNA';
 CHENGS_TASK_2C_FIGURES_CONFIG.CIRC_STAT_FOLDER = 'D:\chengs_task_2c_ca1_project\codes\CircStat2012a';
+CHENGS_TASK_2C_FIGURES_CONFIG.DISTINGUISHABLE_COLORS_FOLDER = 'D:\chengs_task_2c_ca_projects\codes\DistinguishableColors';
 CHENGS_TASK_2C_FIGURES_CONFIG.MAIN_SCRIPTS_FOLDER = 'D:\chengs_task_2c_ca1_project\codes\chengs_task_2c_ca1';
+CHENGS_TASK_2C_FIGURES_CONFIG.CIRCULAR_STATISTICS_TOOLBOX_FOLDER = 'D:\chengs_task_2c_ca1_project\codes\CircularStatisticsToolbox';
+CHENGS_TASK_2C_FIGURES_CONFIG.BOXPLOTGROUP_FOLDER = 'D:\chengs_task_2c_ca1_project\codes\BoxPlotGroup';
 
 % Add the paths we need. These have the common scripts we
 % need in addition to the main analysis.
 addpath(genpath(fullfile(CHENGS_TASK_2C_FIGURES_CONFIG.MAIN_SCRIPTS_FOLDER, 'main_scripts')));
 addpath(genpath(CHENGS_TASK_2C_FIGURES_CONFIG.MULANA_FOLDER));
 addpath(genpath(CHENGS_TASK_2C_FIGURES_CONFIG.CIRC_STAT_FOLDER));
+addpath(genpath(CHENGS_TASK_2C_FIGURES_CONFIG.DISTINGUISHABLE_COLORS_FOLDER));
+addpath(genpath(CHENGS_TASK_2C_FIGURES_CONFIG.CIRCULAR_STATISTICS_TOOLBOX_FOLDER));
+addpath(genpath(CHENGS_TASK_2C_FIGURES_CONFIG.BOXPLOTGROUP_FOLDER));
